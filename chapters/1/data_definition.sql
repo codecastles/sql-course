9718e8652c4b975d31789fa3390dfd2618cfd4ae
tacitphoenix <sondiek@gmail.com>
2020-01-07 17:34:32 -0500

    data definition language

---
+++ chapters/1/data_definition.sql
@@ -0,0 +1,11 @@
+-- create fire table
+create table fire(id int, color varchar(20), degrees numeric);
+
+-- insert type of fire
+insert into fire values(1, 'blue', 92.3);
+
+-- display table
+select * from fire;
+
+-- delete table
+drop table fire;
